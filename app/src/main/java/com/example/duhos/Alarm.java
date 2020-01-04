49e929c5beb46ba4a86c4990ccce7f6c4c642917
Kreso <kresimirtomic1998@gmail.com>
2020-01-04 18:47:17 +0100

    Git

--- app/src/main/java/com/example/duhos/Alarm.java
+++ app/src/main/java/com/example/duhos/Alarm.java
@@ -1,18 +1,13 @@
 package com.example.duhos;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
-import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-
 import io.realm.Realm;
 import io.realm.RealmResults;
 
@@ -20,70 +15,72 @@ public class Alarm extends AppCompatActivity {
 
     Button alarmButton;
     TimePicker timePicker;
-    int sati,minute;
-    Realm realm;
-    List<DatesSelected>resultList;
+    int sati,minute; //spremam u to vrijeme sa sata
+    Realm realm; //baza podataka
+    List<DatesSelected>helpList; //pomocna lista mojih objekata
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm);
-        getSupportActionBar().hide();
-        DatesSelected date = new DatesSelected();
-        ArrayList<DatesSelected> list=new ArrayList<DatesSelected>();
-        realm=Realm.getDefaultInstance();
+        setContentView(R.layout.activity_alarm); //prikazi activity za alarm
+        getSupportActionBar().hide(); //sakrij ime aplikaicije u activityu
+
+        DatesSelected date = new DatesSelected(); //objekt sa datumom i vremenom
+        ArrayList<DatesSelected> list=new ArrayList<DatesSelected>(); //lista  DatesSelected objekata
+
+        realm=Realm.getDefaultInstance(); //inicijalizacije Realm-a
         realm.executeTransaction(realm1 -> {
-            resultList=realm1.where(DatesSelected.class).findAll();
-            list.addAll(resultList);
+            helpList=realm1.where(DatesSelected.class).findAll();
+            list.addAll(helpList); //puni listu prema podatcima iz baze podataka
         });
 
-        String datum=getIntent().getStringExtra("datum");
-        List<String>datumi=getIntent().getStringArrayListExtra("lista");
+        String datum=getIntent().getStringExtra("datum"); //spremi kliknuti datum koji je poslan putem itenta
+        List<String>datumi=getIntent().getStringArrayListExtra("lista"); //spremi listu datuma koja je poslana putem itenta
 
         alarmButton=findViewById(R.id.idAlarmButton);
         timePicker=findViewById(R.id.idTimePicker);
 
         int flag=0;
-        for(int i=0;i<datumi.size();i++){
+        for(int i=0;i<datumi.size();i++){  //ako oznaceni datum postoji u listi datuma postavi zastavu na 1
             if(datumi.get(i).equals(datum))
                 flag=1;
         }
 
-        if(flag==0) {
+        if(flag==0) { //ako nema oznacenog datuma u listi
             alarmButton.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
-                    sati = timePicker.getHour();
-                    minute = timePicker.getMinute();
-                    realm = Realm.getDefaultInstance();
+                public void onClick(View v) { //na klik
+                    sati = timePicker.getHour(); //spremi sate
+                    minute = timePicker.getMinute(); //spremi minute
+                    realm = Realm.getDefaultInstance(); //pokreni bazu
                     realm.beginTransaction();
-                    date.setDatum(datum);
-                    date.setSati(sati);
-                    date.setMinute(minute);
-                    realm.copyToRealm(date);
+                    date.setDatum(datum); //spremi u objekt datum
+                    date.setSati(sati); //spremi u objekt sate
+                    date.setMinute(minute); //spremi u objekt minute
+                    realm.copyToRealm(date); //spremi objekt u bazu
                     realm.commitTransaction();
                     realm.close();
-                    Intent intent = new Intent(Alarm.this, CalendarClass.class);
+                    Intent intent = new Intent(Alarm.this, CalendarClass.class); //otvori kalendar
                     startActivity(intent);
-                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out); //fade_in prijelaz
 
                 }
             });
         }
 
-        if(flag==1){
+        if(flag==1){ //ako je oznaceni datum u listi
             alarmButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 
-                    if(!realm.isEmpty()){
+                    if(!realm.isEmpty()){ //ako baza nije prazna
                         RealmResults<DatesSelected> dates = realm
                                 .where(DatesSelected.class)
                                 .findAll();
 
                         DatesSelected userdatabase = dates
                                 .where()
-                                .equalTo("datum",datum)
+                                .equalTo("datum",datum) //nadji gdje se nalazi taj datum sa starim vremenom
                                 .findFirst();
 
                         if(userdatabase!=null) {
@@ -92,13 +89,13 @@ public class Alarm extends AppCompatActivity {
                                 realm.beginTransaction();
                             }
 
-                            userdatabase.deleteFromRealm();
-                            resultList=dates;
+                            userdatabase.deleteFromRealm(); //izbrisi iz baze
+                            helpList=dates;
                             realm.commitTransaction();
                         }
 
                     }
-
+                    //nakon sto je datum izbrisan dodaj ga ponovno sa novim vremenom
                     sati = timePicker.getHour();
                     minute = timePicker.getMinute();
                     realm = Realm.getDefaultInstance();
--- app/src/main/java/com/example/duhos/CalendarClass.java
+++ app/src/main/java/com/example/duhos/CalendarClass.java
@@ -23,18 +23,18 @@ import java.util.Locale;
 public class CalendarClass extends AppCompatActivity {
 
     private static final String TAG ="TAG";
-    int count = 0;
-    int br=0;
+    int count = 0; //brojac znakova '&'
+    int br=0; //brojac dogadjaja
 
+    //liste za događaje
     List<String> Datumi=new ArrayList<String>();
     List<String> Nazivi=new ArrayList<String>();
     List<String> Vremena=new ArrayList<String>();
     List<String> Mjesta=new ArrayList<String>();
 
-    List<Date> dates=new ArrayList<Date>();
+    List<Date> dates=new ArrayList<Date>(); //lista datuma u obliku datuma
 
-    List<String> exDatesString=new ArrayList<String>();
-
+    List<String> exDatesString=new ArrayList<String>(); //lista proslih datuma unazad jednu godinu
 
                 @Override
                 protected void onCreate(Bundle savedInstanceState) {
@@ -42,85 +42,80 @@ public class CalendarClass extends AppCompatActivity {
                     getSupportActionBar().hide();
 
                     setContentView(R.layout.activity_calendar);
-                    Calendar nextYear = Calendar.getInstance();
-                    nextYear.add(Calendar.YEAR, 1);
 
                     CalendarPickerView calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
-                    Date today = new Date();
 
+                    Date today = new Date(); //danasnji datum
+                    Calendar nextYear = Calendar.getInstance();
+                    nextYear.add(Calendar.YEAR, 1); //prikazi kalendar za jednu godinu unaprijed
+                    calendar.init(today, nextYear.getTime()) //kalendar od danas do sljedece godine
+                            .withSelectedDate(today);
 
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy"); //format datuma koji trebamo
 
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-
-
-                    for(int i=1;i<366;i++) {
-                        SimpleDateFormat dateFormat1 = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                    for(int i=1;i<366;i++) { //dodaj 365 vrijednosti u listu
                         Calendar calendar1 = Calendar.getInstance();
                         calendar1.setTime(new Date());
-                        calendar1.add(Calendar.DATE, -i);
+                        calendar1.add(Calendar.DATE, -i); //po jedan dan unazad dodaj u listu
                         String yesterdayAsString = dateFormat.format(calendar1.getTime());
                         exDatesString.add(yesterdayAsString);
                     }
-                    //Toast.makeText(getApplication().getApplicationContext(),yesterday.toString(),Toast.LENGTH_SHORT).show();
 
-                    Date mydate2 = new Date();
                     SimpleDateFormat dateFormat2 = new SimpleDateFormat("dd/MM/yyyy");
-                    String today1 = dateFormat2.format(mydate2);
+                    String danas = dateFormat2.format(today); //danasnji datum u formatu koji trebamo
 
-                    calendar.init(today, nextYear.getTime())
-                            .withSelectedDate(today);
+                    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance(); //instanciranje FireBase baze podataka
+                    DatabaseReference mDbRef; //referenca
 
-                    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-                    DatabaseReference mDbRef;
-
-                    for(int i=0;i<365;i++) {
-                    mDbRef = mDatabase.getReference("Događaji/").child(exDatesString.get(i));
-                    mDbRef.removeValue();
+                    for(int i=0;i<365;i++) { //za 365 dana
+                    mDbRef = mDatabase.getReference("Događaji/").child(exDatesString.get(i)); //gledaj jel postoji dijete u bazi koje se zove kao datum koji je prosao
+                    mDbRef.removeValue();                                                           //ako ima izbrisi iz baze
                     }
 
-                    mDbRef = mDatabase.getReference("Događaji/");
+                    mDbRef = mDatabase.getReference("Događaji/"); //fokusiraj se na dogadjaje u bazi
 
                     mDbRef.addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
-                            String value = dataSnapshot.getValue().toString();
+                            String podatakDogadjaja = dataSnapshot.getValue().toString(); //svi događaji su predsatvljeni jednim stringom i nekim znakovima
 
-                            value = value.replace("{", "&");
-                            value = value.replace("}", "&");
-                            value = value.replace("=", "");
-                            value = value.replace(",", "");
-                            value = value.replace("Vrijeme", "&");
-                            value = value.replace("Naziv", "&");
-                            value = value.replace("Mjesto", "&");
-                            value = value.replace("&&", "&");
-                            value = value.replace(" &", "&");
-                            value = value.replace("& ", "&");
+                            //uredi string tako da zamjenimo nepotrebne znakove i lakse procitamo zapis
+                            podatakDogadjaja = podatakDogadjaja.replace("{", "&");
+                            podatakDogadjaja = podatakDogadjaja.replace("}", "&");
+                            podatakDogadjaja = podatakDogadjaja.replace("=", "");
+                            podatakDogadjaja = podatakDogadjaja.replace(",", "");
+                            podatakDogadjaja = podatakDogadjaja.replace("Vrijeme", "&");
+                            podatakDogadjaja = podatakDogadjaja.replace("Naziv", "&");
+                            podatakDogadjaja = podatakDogadjaja.replace("Mjesto", "&");
+                            podatakDogadjaja = podatakDogadjaja.replace("&&", "&");
+                            podatakDogadjaja = podatakDogadjaja.replace(" &", "&");
+                            podatakDogadjaja = podatakDogadjaja.replace("& ", "&");
 
 
-                            for (int i = 0; i < value.length(); i++) {
-                                if (value.charAt(i) == '&') {
+                            for (int i = 0; i < podatakDogadjaja.length(); i++) { //prolazi podatkom dogadjaja i prebroj znakove '&'
+                                if (podatakDogadjaja.charAt(i) == '&') {
                                     count++;
                                 }
                             }
 
                             for (int i = 1; i < count; i = i + 4) {
-                                Datumi.add(value.split("&")[i]);
+                                Datumi.add(podatakDogadjaja.split("&")[i]); //iza svakog četvrtog znaka '&' se nalazi datum
                                 br++;
                             }
 
                             for (int i = 2; i < count; i = i + 4) {
-                                Vremena.add(value.split("&")[i]);
+                                Vremena.add(podatakDogadjaja.split("&")[i]); //iza svakog četvrtog znaka '&' se nalazi vrijeme
                             }
 
                             for (int i = 3; i < count; i = i + 4) {
-                                Mjesta.add(value.split("&")[i]);
+                                Mjesta.add(podatakDogadjaja.split("&")[i]); //iza svakog četvrtog znaka '&' se nalazi mjesto
                             }
 
                             for (int i = 4; i < count; i = i + 4) {
-                                Nazivi.add(value.split("&")[i]);
+                                Nazivi.add(podatakDogadjaja.split("&")[i]); //iza svakog četvrtog znaka '&' se nalazi naziv
                             }
 
-                            for (int i = 0; i < br; i++) {
+                            for (int i = 0; i < br; i++) { //svim datumima u listi promijeni format ('-' -> '/')
                                 String temp;
                                 temp=Datumi.get(i).toString();
                                 temp = temp.replace("-", "/");
@@ -130,36 +125,34 @@ public class CalendarClass extends AppCompatActivity {
                             }
 
                             for(int i=0;i<br;i++) {
-                                Date date1 = new Date();
+                                Date noviDatum = new Date();
                                 try {
-                                    date1 = new SimpleDateFormat("dd/MM/yyyy").parse(Datumi.get(i).toString());
-                                    System.out.println(date1);
+                                    noviDatum = new SimpleDateFormat("dd/MM/yyyy").parse(Datumi.get(i).toString()); //pretvori String u Date
+                                    System.out.println(noviDatum);
                                 } catch (ParseException e) {
                                     e.printStackTrace();
                                 }
-                                dates.add(date1);
-                                calendar.highlightDates(dates);
+                                dates.add(noviDatum); //napuni listu Date-ova
+                                calendar.highlightDates(dates); //oznaci na kalendari sve datume iz liste dates
                             }
 
                             for(int i=0;i<br;i++){
-                                if(Datumi.get(i).equals(today1))
+                                if(Datumi.get(i).equals(danas)) //ako je kliknuti datum danas, otvori automatski njegove informacije
                                     openDialog(Datumi.get(i).toString(),Nazivi.get(i).toString(),Mjesta.get(i).toString(),Vremena.get(i).toString());
                             }
 
-                            calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+                            calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() { //na kliknuti datum
                                 @Override
-                                public void onDateSelected(Date date) {
+                                public void onDateSelected(Date datum) {
                                     int flag=0;
-                                    if(flag==0) {
                                         for (int i = 0; i < br; i++) {
-                                            if (dates.get(i).equals(date)) {
-                                                //Toast.makeText(getApplication().getApplicationContext(),Nazivi.get(i).toString(),Toast.LENGTH_SHORT).show();
+                                            if (dates.get(i).equals(datum)) {  //ako kliknuti datum postoji u listi koji imaju dogadjaje otvori informacije
                                                 flag = 1;
                                                 openDialog(Datumi.get(i).toString(),Nazivi.get(i).toString(),Mjesta.get(i).toString(),Vremena.get(i).toString());
                                             }
                                         }
-                                    }
-                                    if(flag==0){
+
+                                    if(flag==0){ //ako kliknuti datum ne postoji u listi koji imaju dogadjaje napisi da nema dogadjaja
                                         Toast.makeText(getApplication().getApplicationContext(),"Nema događaja",Toast.LENGTH_SHORT).show();
                                     }
                                 }
@@ -173,23 +166,24 @@ public class CalendarClass extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError error) {
-// Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            // ako se ne uspije procitat iz baze
+                Log.w(TAG, "Greška u čitanju iz baze podataka", error.toException());
             }
         });
 
     }
 
+    //otvori dialog
     private void openDialog(String datum,String naziv,String mjesto,String vrijeme) {
                     Exampledialog exampledialog=new Exampledialog();
-                    exampledialog.show(getSupportFragmentManager(),datum + "&" + naziv + "&" + mjesto + "&" + vrijeme+"&");
+                    exampledialog.show(getSupportFragmentManager(),datum + "&" + naziv + "&" + mjesto + "&" + vrijeme+"&");//kao tag stavi informacije kliknutog dogadjaja da se mogu dohvatiti iz dialoga
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed() { //ako se stisne tipka unazad vrati se na MainActivity
         Intent intent = new Intent(CalendarClass.this,MainActivity.class);
         startActivity(intent);
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out); //efekt
 
     }
 
