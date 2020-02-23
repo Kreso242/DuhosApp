719029d629b8b6005f420bef9c193bf62434d5b3
Kreso <kresimirtomic1998@gmail.com>
2020-02-23 17:34:12 +0100

    Merge branches 'master' and 'srcaPitanja' of https://github.com/Kreso242/DuhosApp
    # Conflicts:
    #	app/src/main/java/com/example/duhos/RecyclerViewAdapter.java

--- app/src/main/java/com/example/duhos/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/duhos/RecyclerViewAdapter.java
@@ -1,17 +1,24 @@
 package com.example.duhos;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableContainer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearSmoothScroller;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 
 
 import static com.example.duhos.R.drawable.clicked_item_list;
@@ -19,29 +26,27 @@ import static com.example.duhos.R.drawable.list_item_background;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
-    private ArrayList<String> pitanja= new ArrayList<String>();
+    private ArrayList<String> pitanja = new ArrayList<String>();
     private ArrayList<String> odgovori = new ArrayList<String>();
     private Context context;
-    private int srcePosition;
-    private HashMap<Integer, Boolean> srce = new HashMap<>();
-    //    private ArrayList<Boolean> sr = new ArrayList<>();
-    private boolean srceShow = false;
+    private ArrayList<Boolean> heart = new ArrayList<>();
+
     private int pitanjePosition;
     private boolean pitanjeShow = false;
-    private ArrayList<String> listaFavorita= new ArrayList<String>();
 
 
-    public RecyclerViewAdapter(ArrayList<String> pitanja,ArrayList<String> odgovori, Context context) {
+    public RecyclerViewAdapter(ArrayList<String> pitanja, ArrayList<String> odgovori, ArrayList<Boolean> heart, Context context) {
         this.pitanja = pitanja;
         this.odgovori = odgovori;
+        this.heart = heart;
         this.context = context;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem,parent,false);
-        ViewHolder holder=new ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
         return holder;
     }
 
@@ -51,81 +56,54 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.textViewOdgovori.setText(odgovori.get(position));
         //  holder.textViewBrojevi.setText(String.valueOf(position+1)+".");
 
-        //TEST
+        final boolean hr = heart.get(position);
 
-//        Iterator it = srce.entrySet().iterator();
-//        while (it.hasNext()) {
-//            Map.Entry pair = (Map.Entry)it.next();
-//            if(/*pair.getKey().equals(position) &&*/ pair.getValue().equals(true)){
-////                Toast.makeText(context,String.valueOf(position),Toast.LENGTH_SHORT).show();
-//                holder.imageButtonPunoSrce.setVisibility(View.VISIBLE);
-//                holder.imageButtonPraznoSrce.setVisibility(View.GONE);
-//            }
-//            else {
-//                holder.imageButtonPunoSrce.setVisibility(View.GONE);
-//                holder.imageButtonPraznoSrce.setVisibility(View.VISIBLE);
-//            }
-//        }
+        holder.imageButtonPunoSrce.setVisibility(hr ? View.VISIBLE : View.GONE);//ako je true postavi puno srce, ako false onda postavi prazno srce
+        holder.imageButtonPraznoSrce.setVisibility(hr ? View.GONE : View.VISIBLE);//obrnuto od ovog gore
 
 
-        if (pitanjePosition == position && pitanjeShow){
+        if (pitanjePosition == position && pitanjeShow) {
             holder.textViewOdgovori.setVisibility(View.VISIBLE);
             holder.textViewPitanja.setTextColor(Color.parseColor("#FFFFFF"));
             holder.textViewOdgovori.setTextColor(Color.parseColor("#FFFFFF"));
             holder.isExpanded = true;
-            holder. relativeLayout.setBackground(context.getResources().getDrawable(clicked_item_list));
-        }else{
+            holder.relativeLayout.setBackground(context.getResources().getDrawable(clicked_item_list));
+        } else {
             holder.textViewOdgovori.setVisibility(View.GONE);
             holder.textViewPitanja.setTextColor(Color.parseColor("#000000"));
             holder.isExpanded = false;
             holder.relativeLayout.setBackground(context.getResources().getDrawable(list_item_background));
         }
 
-        holder.imageButtonPraznoSrce.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listaFavorita.add(pitanja.get(position));
-
-            }
-        });
-
         holder.imageButtonPunoSrce.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for(int i=0;i<listaFavorita.size();i++){
-                    if(listaFavorita.get(i)==pitanja.get(position))
-                        listaFavorita.remove(i);
-                }
+                heart.set(position, false);
+                holder.imageButtonPunoSrce.setVisibility(hr ? View.VISIBLE : View.GONE);
+                holder.imageButtonPraznoSrce.setVisibility(hr ? View.GONE : View.VISIBLE);
+                notifyDataSetChanged();
             }
         });
-
+        holder.imageButtonPraznoSrce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                heart.set(position, true);
+                holder.imageButtonPunoSrce.setVisibility(hr ? View.VISIBLE : View.GONE);
+                holder.imageButtonPraznoSrce.setVisibility(hr ? View.GONE : View.VISIBLE);
+                notifyDataSetChanged();
+            }
+        });
 
         holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(holder.isExpanded==false) {
-//                    holder.textViewOdgovori.setVisibility(View.VISIBLE);
-//                    holder.textViewPitanja.setTextColor(Color.parseColor("#FFFFFF"));
-//                    holder.textViewOdgovori.setTextColor(Color.parseColor("#FFFFFF"));
-//                    holder.isExpanded=true;
-//                    holder. relativeLayout.setBackground(context.getResources().getDrawable(clicked_item_list));
-
+                if (holder.isExpanded == false) {
                     pitanjePosition = position;
                     pitanjeShow = true;
                     notifyDataSetChanged();
-
-//                    Toast.makeText(context,String.valueOf(position),Toast.LENGTH_SHORT).show();
-                }
-                else if(holder.isExpanded==true){
-//                    holder.textViewOdgovori.setVisibility(View.GONE);
-//                    holder.textViewPitanja.setTextColor(Color.parseColor("#000000"));
-//                    holder.isExpanded=false;
-//                    holder.relativeLayout.setBackground(context.getResources().getDrawable(list_item_background));
-
+                } else if (holder.isExpanded == true) {
                     pitanjeShow = false;
                     notifyDataSetChanged();
-
-//                    Toast.makeText(context,String.valueOf(position),Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -136,7 +114,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return pitanja.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView textViewPitanja;
         TextView textViewOdgovori;
@@ -144,16 +122,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         RelativeLayout relativeLayout;
         ImageButton imageButtonPunoSrce;
         ImageButton imageButtonPraznoSrce;
-        private boolean isExpanded=false;
+        private boolean isExpanded = false;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            textViewPitanja=itemView.findViewById(R.id.idPitanje);
-            textViewOdgovori=itemView.findViewById(R.id.idOdgovor);
-            relativeLayout=itemView.findViewById(R.id.parentLayout);
+            textViewPitanja = itemView.findViewById(R.id.idPitanje);
+            textViewOdgovori = itemView.findViewById(R.id.idOdgovor);
+            relativeLayout = itemView.findViewById(R.id.parentLayout);
             // textViewBrojevi=itemView.findViewById(R.id.redniBrojPitanja);
-            imageButtonPraznoSrce=itemView.findViewById(R.id.praznoSrce);
-            imageButtonPunoSrce=itemView.findViewById(R.id.punoSrce);
+            imageButtonPraznoSrce = itemView.findViewById(R.id.praznoSrce);
+            imageButtonPunoSrce = itemView.findViewById(R.id.punoSrce);
 
         }
     }
