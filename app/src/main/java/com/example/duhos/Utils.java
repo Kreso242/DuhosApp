d40e939f14dcc42b88e45e27dd13227bd5bf8de6
Kreso <kresimirtomic1998@gmail.com>
2020-02-26 14:52:13 +0100

    Merge branches 'master' and 'srcaPitanja' of https://github.com/Kreso242/DuhosApp
    # Conflicts:
    #	app/src/main/java/com/example/duhos/RecyclerViewAdapter.java

--- app/src/main/java/com/example/duhos/Utils.java
+++ app/src/main/java/com/example/duhos/Utils.java
@@ -2,6 +2,6 @@ package com.example.duhos;
 
 public class Utils {
 
-    public static final String EMAIL="kresimirtomic1998@gmail.com";
-    public static final String PASSWORD="bARCELONa10";
+    public static final String EMAIL="pitanja.duhos@gmail.com";
+    public static final String PASSWORD="pitanjaodgovori20";
 }
