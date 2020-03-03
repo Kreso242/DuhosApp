867decd8125a71b7570c75384ffe1f4c7b3f97fe
Kreso <kresimirtomic1998@gmail.com>
2020-03-03 19:24:35 +0100

    Merge branches 'master' and 'srcaPitanja' of https://github.com/Kreso242/DuhosApp
    # Conflicts:
    #	app/src/main/java/com/example/duhos/RecyclerViewAdapter.java

--- app/src/main/java/com/example/duhos/Multimedia.java
+++ app/src/main/java/com/example/duhos/Multimedia.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hitomi.cmlibrary.CircleMenu;
 
@@ -23,4 +24,31 @@ public class Multimedia extends AppCompatActivity {
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
 
     }
+
+    public void goMolitva(View view) {
+        Intent intent = new Intent(Multimedia.this, Molitva.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+    public void goKalendar(View view) {
+        Intent intent = new Intent(Multimedia.this, CalendarClass.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+    public void goPitanja(View view) {
+        Intent intent = new Intent(Multimedia.this, Questions.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+    public void goMultimedija(View view) {
+        Intent intent = new Intent(Multimedia.this, Multimedia.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+    public void goPjesmarica(View view) {
+        Intent intent = new Intent(Multimedia.this, Songs.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
 }
--- app/src/main/java/com/example/duhos/Songs.java
+++ app/src/main/java/com/example/duhos/Songs.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hitomi.cmlibrary.CircleMenu;
 
@@ -24,4 +25,31 @@ public class Songs extends AppCompatActivity {
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
 
     }
+
+    public void goMolitva(View view) {
+        Intent intent = new Intent(Songs.this, Molitva.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+    public void goKalendar(View view) {
+        Intent intent = new Intent(Songs.this, CalendarClass.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+    public void goPitanja(View view) {
+        Intent intent = new Intent(Songs.this, Questions.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+    public void goMultimedija(View view) {
+        Intent intent = new Intent(Songs.this, Multimedia.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+    public void goPjesmarica(View view) {
+        Intent intent = new Intent(Songs.this, Songs.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
 }
